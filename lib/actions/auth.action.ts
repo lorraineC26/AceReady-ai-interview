22ea342c8c72de09f2509dc79d229e2585d11ad6
lorraineC26 <lorraine.quinyee@gmail.com>
2026-03-10 16:01:47 -0400

    feat: build a fx to handle sign-in cookie generation

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,7 +1,10 @@
 // whenever in an action file, have to add 'use server' at the top
 "use server";
 
-import { db } from "@/firebase/admin";
+import { db, auth } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
@@ -39,4 +42,23 @@ export async function signUp(params: SignUpParams) {
       message: 'Failed to create an account.'
     }
   }
+}
+
+// this fx is used to generate the cookie that will be sent to the client after successful sign-in or sign-up, so can stay authenticated in subsequent requests
+export async function setSesstionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000, // 7 days in milliseconds
+  }); 
+
+  // Set the session cookie in the response headers
+  cookieStore.set('session', sessionCookie, {
+    maxAge: ONE_WEEK, 
+    httpOnly: true, // Cookie is only accessible via HTTPS
+    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    path: '/', // Cookie is valid for the entire site
+    sameSite: 'lax', // CSRF protection
+  })
+
 }
